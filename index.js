876f8b57ea1e0589e97cda608ec04027cccdc6bf
Ruta <ruta@email>
2024-03-14 17:06:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// create dog age to human age formula
+
+const dogAgeInput = prompt("What's your dog age?");
+const humanAge = (dogAgeInput - 2) * 4 + 21;
+alert("Your dog is " + humanAge + " years in human age.");
